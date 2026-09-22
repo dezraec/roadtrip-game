7a9a7d7bb31d154139f2499c12f897e85ab2085c
Samprisho <samuel.esho@protonmail.com>
2026-09-22 15:30:34 -0500

    Git doing some wacky stuff...fixed it! Hopefully...

---
+++ road_spikes.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+var van_ragdoll_scn = preload("res://van_ragdoll.tscn")
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D and body.name == "van":
+		
+		if not is_instance_valid(body):
+			return
+		
+		body.hide()
+		
+		var ragdoll: RigidBody2D = van_ragdoll_scn.instantiate()
+		get_tree().current_scene.add_child(ragdoll)
+		
+		await get_tree().process_frame
+		
+		ragdoll.global_position = body.global_position + Vector2(0, -50)
+		body.process_mode = Node.PROCESS_MODE_DISABLED
+		
+	
+		
+		var impulse = Vector2.from_angle(-PI/4 + randf() * PI/8) * 100
+		
+		print(impulse)
+		
+		ragdoll.apply_impulse(impulse)
+		ragdoll.apply_torque_impulse(700)
+		
+		await get_tree().create_timer(1).timeout
+		
+		get_tree().change_scene_to_file("res://gameover.tscn")
